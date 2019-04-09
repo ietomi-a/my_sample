3d75395ee4d1875737d59fb183eb1b569b86987d
aie2 <45744844+aie2@users.noreply.github.com>
2019-04-09 17:00:44 +0900

    modify

--- src/components/Ranking.tsx
+++ src/components/Ranking.tsx
@@ -9,24 +9,24 @@ const RankImage = ( {image_path, rate} ) => (
 );
 
 const _Ranking = (props) => {
-  console.log( "in _Ranking props=", props );
-  //let image_paths = Object.keys(props.rates);
-  // let tmp = image_paths.map( (image_path) => ( props.rates[image_path] ) );
+  //console.log( "in _Ranking props.rates=", props.ratas );
+  let image_paths = Object.keys(props.rates);
+  //let tmp = image_paths.map( (image_path) => ( props.rates[image_path] ) );
   // console.log( "in _Ranking image_paths=", image_paths );
-  // return (<ul>
-  //         {image_paths.map( image_path =>
-  //                           <RankImage image_path={image_path}
-  //                                      rate={props.rates[image_path]} />
-  //                   )}
-  //         </ul> );
+  return (<ul>
+          {image_paths.map( image_path =>
+                            <RankImage image_path={image_path}
+                                       rate={props.rates[image_path]} />
+                          )}
+          </ul> );
   //  return (<div> {props.rates["images/0.jpg"]}</div>);
-    return (<div> my test</div>);
+  //return (<div> my test</div>);
 };
 
 
 function _mapStateToProps_ranking (state) {
-  console.log("hello, _ranking mapState, state =", state );
-  return state.ranking.rates ;
+  // console.log("hello, _ranking mapState, state =", state );
+  return { rates: state.ranking.rates };
 };
 
 const Ranking = connect( _mapStateToProps_ranking)(_Ranking);
